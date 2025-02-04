1150df39df26b59202e1f9fd165bad175103a1f5
loganteeps <loganteeps@gmail.com>
2025-02-04 11:00:04 -0500

    Update rejection test.py

--- Electrical 24-25/groundstation/rejection test.py
+++ Electrical 24-25/groundstation/rejection test.py
@@ -16,23 +16,23 @@ boundary_coords : List[Tuple[float, float]] = [(lon, lat) for lat, lon in flippe
 
 boundary_polygon = Polygon(boundary_coords)
 
-# Define the point
-current_point = Point(-76.5425, 38.3143)
-
-track_vector = np.array([0,-1])
+# Define the inputs
+current_point = Point(-76.5485, 38.3153)
+track_vector = np.array([0,1])
+goal_point = Point(-76.5425, 38.3143)
+goal = np.array([goal_point.x, goal_point.y])
 normalized_vector = track_vector/np.linalg.norm(track_vector)
 
-def calculate_net_boundary_vector(point:Point, vector:np.ndarray[float, float], polygon:Polygon) -> np.ndarray[float, float]:
+def calculate_net_boundary_vector(point:Point, goalpoint:Point, vector:np.ndarray[float, float], polygon:Polygon) -> np.ndarray[float, float]:
     """
     Calculate a net vector away from multiple boundary edges of a polygon.
     """
     boundary_coords = list(polygon.exterior.coords)
     net_vector = np.array([0.0, 0.0])   
-    number_points = 4
     distance = [0] * (len(boundary_coords) - 1)
 
     #find 3 closest points
-    threshold = 0.003
+    threshold = 0.003 # Threshold distance to consider
     for i in range(len(boundary_coords) - 1):  # Iterate through edges
         edge = LineString([boundary_coords[i], boundary_coords[i + 1]])
         nearest_point = edge.interpolate(edge.project(point))
@@ -45,9 +45,7 @@ def calculate_net_boundary_vector(point:Point, vector:np.ndarray[float, float], 
         if distance > threshold:
             continue
 
-        # this is where I'm adding the angle component to the rejection vector.
-        edge_vector = np.array([boundary_coords[i + 1][0]-boundary_coords[i][1], boundary_coords[i + 1][1]-boundary_coords[i][1]])   
-        length_edge = np.linalg.norm(edge_vector) 
+        # this is where I'm adding the angle component to the rejection vector
         length_vector = np.linalg.norm(vector)
 
         # Find vector from nearest point to current point
@@ -60,12 +58,6 @@ def calculate_net_boundary_vector(point:Point, vector:np.ndarray[float, float], 
         vectortowall = point_array - nearest_point_array
         costheta = np.dot(vectortowall, normalized_vector)
         angle_weight = ((costheta + 1)**2)/4
-
-        #see if plane is nearing wall
-        new_point = point_array + normalized_vector
-        distance_prime = new_point - nearest_point_array
-        distance_prime = np.hypot(distance_prime[0], distance_prime[1])
-
         
 
         # Distance between current point and wall and closure rate
@@ -74,21 +66,28 @@ def calculate_net_boundary_vector(point:Point, vector:np.ndarray[float, float], 
         
         
         if distance > 0:
-            vector = vectortowall*angle_weight/(distance**1.7)  #Scale the vector
-            plt.plot(nearest_point.x, nearest_point.y, 'go')  # Nearest point
-            plt.arrow(nearest_point.x, nearest_point.y, vector[0], vector[1], head_width=0.001, head_length=0.001,linewidth=.00001, fc='green', ec='green')
+            vector = vectortowall*angle_weight/((distance**1.7))  #Scale the vector
+            plt.plot(nearest_point.x, nearest_point.y, 'ro')  # Nearest point
+            plt.arrow(nearest_point.x, nearest_point.y, vector[0], vector[1], head_width=0.001, head_length=0.001,linewidth=.00001, fc='red', ec='red')
 
             # Add to the net vector
             net_vector += vector
     
+    goal = np.array([goal_point.x, goal_point.y])
+    goal = goal - point_array 
+    # Combine the vectors
+    weight_repulsion = 0.00001
+    weight_goal = .7
+    net_vector = weight_repulsion*net_vector+weight_goal*goal
+
     return net_vector
 
 # Compute the net vector
-net_vector = calculate_net_boundary_vector(current_point, track_vector, boundary_polygon)
+net_vector = calculate_net_boundary_vector(current_point, goal_point, track_vector, boundary_polygon)
 
 # Output results
 print(f"Point: {current_point}")
-print(f"Net Vector Away from Boundary: {net_vector}")
+print(f"Net Vector: {net_vector}")
 
 
 # Plot the polygon
@@ -100,11 +99,11 @@ plt.plot(x, y, 'b-', label="Boundary")
 
 
 # Plot the current point
-plt.plot(current_point.x, current_point.y, 'ro', label="Current Point")
-
+plt.plot(current_point.x, current_point.y, 'bo', label="Current Point")
+plt.plot(goal_point.x, goal_point.y, 'go', label="Destination Point")
 
 # Draw the net vector and track vector
-plt.arrow(current_point.x, current_point.y, net_vector[0], net_vector[1], head_width=0.001, head_length=0.001, linewidth=.00001, fc='red', ec='red', label="Net Vector")
+plt.arrow(current_point.x, current_point.y, net_vector[0], net_vector[1], head_width=0.001, head_length=0.001, linewidth=.00001, fc='green', ec='green', label="Net Vector")
 plt.arrow(current_point.x, current_point.y, normalized_vector[0], normalized_vector[1], head_width=0.001, head_length=0.001, linewidth=.00001, fc='blue', ec='blue', label="Track Vector")
 
 plt.axis('equal')
